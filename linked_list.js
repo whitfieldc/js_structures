64db0463c8f0864e03edbac6a931e8b2195dc708
Charles <whitfield.cw@gmail.com>
2016-01-19 21:54:03 -0800

    add LinkedList wrapper class

--- linked_list.js
+++ linked_list.js
@@ -12,3 +12,11 @@ var start = new Node("beginning", end);
 
 console.log(start.value);
 console.log(start.next);
+
+var LinkedList = function(firstNode){
+  this.head = firstNode;
+}
+
+var listOne = new LinkedList(start);
+
+console.log(listOne.head);
