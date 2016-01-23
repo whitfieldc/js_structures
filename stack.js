7d7b7e7c7a74397da516a408ace9e030256766a8
Charles Whitfield <whitfield.cw@gmail.com>
2016-01-22 16:09:19 -0800

    add pop function to stack

--- stack.js
+++ stack.js
@@ -6,6 +6,12 @@ var Stack = function(initialValue){
 
 Stack.prototype.push = function(newVal) {
   this.stack.unshift(newVal);
+  return this.stack;
+}
+
+Stack.prototype.pop = function() {
+  this.stack.shift();
+  return this.stack;
 }
 
 var hello = new Stack("goodbye");
@@ -14,3 +20,5 @@ console.log(hello);
 hello.push("goodbye again");
 console.log(hello);
 console.log(hello.stack);
+hello.pop();
+console.log(hello);
